80ea1e032726ced8b64afc806ca35ee4ca32ad5c
Denis <denis02022007@mail.ru>
2026-07-06 04:13:56 +0300

    feat: create app entry point with empty QMainWindow

---
+++ main.py
@@ -0,0 +1,1 @@
+import sysfrom PyQt5.QtWidgets import QApplication, QMainWindowdef main():    app = QApplication(sys.argv)    window = QMainWindow()    window.setWindowTitle("Калькулятор скидок")    window.resize(800, 600)    window.show()    sys.exit(app.exec_())if __name__ == "__main__":    main()
